f9bd673528fc9380d7fe4d1fe3ee56576fed8e17
abolengio <aboytsov@gumtree.com>
2014-03-12 00:22:49 +0000

    added some todos

--- src/main/java/com/ab/cart/repository/impl/CsvFileProductProvider.java
+++ src/main/java/com/ab/cart/repository/impl/CsvFileProductProvider.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
 
+//todo rename to Reader
 public class CsvFileProductProvider {
 
     public static final String PRODUCT_CSV_FILE_PATH_PROPERTY = "product.catalogue.file";
@@ -27,12 +28,14 @@ public class CsvFileProductProvider {
         this.productCsvEntryParser = productCsvEntryParser;
     }
 
+    //todo rename to read()
     public List<Product> parse() throws IOException {
         String productCsvFilePath = environment.getProperty(PRODUCT_CSV_FILE_PATH_PROPERTY);
         Reader reader = fileReaderProvider.getFileReader(productCsvFilePath);
         CSVReader<Product> csvPersonReader = new CSVReaderBuilder<Product>(reader).strategy(CSVStrategy.UK_DEFAULT)
                 .entryParser(productCsvEntryParser).build();
         List<Product> products = csvPersonReader.readAll();
+        //todo close the reader and test for closure
         return products;
     }
 }
